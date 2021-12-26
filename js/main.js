33e916c5e508c49c0ac45affa58a55c117c241ef
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2021-12-26 23:18:15 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
 new fullpage('#fullPage', {
-	autoScrolling: true,
-	scrollHorizontally: true
 
-	
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.querySelector('.form')
-	form.addEventListener('submit', formSend);
-
-	async function formSend(e) {
-		e.preventDefault();
-		let formData = new FormData(form)
-	
-
-	}
-
-
-
-});
